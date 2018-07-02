efaa0a2a1124f926a801a9fb692c7aad9756c54f
X Norman <xnorman@fb.com>
2018-07-02 14:28:48 -0700

    Creates custom Row Layout

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -1,4 +1,24 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Tweet {
+
+    //List attributes
+    public String body, createdAt;
+    public User user;
+    public long uid; //database ID for the tweet
+
+    //deserialize JSON
+    public static Tweet fromJSON(JSONObject object) throws JSONException {
+        Tweet tweet = new Tweet();
+
+        tweet.body = object.getString("text");
+        tweet.uid = object.getLong("id");
+        tweet.createdAt = object.getString("created_at");
+        tweet.user = User.fromJSON(object.getJSONObject("user"));
+
+        return tweet;
+    }
 }
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -0,0 +1,23 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class User {
+
+    //list attributes
+    public String name, screenName, profileImageUrl;
+    public long uid;
+
+    //deserialize
+    public static User fromJSON(JSONObject json) throws JSONException{
+        User user = new User();
+
+        user.name = json.getString("name");
+        user.screenName = json.getString("screen_name");
+        user.profileImageUrl = json.getString("profile_image_url");
+        user.uid = json.getLong("id");
+
+        return user;
+    }
+}
